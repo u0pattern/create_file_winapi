f612a3a065485dc400b5ac0a45eec6d3f5cd4584
ムハンマド <u9cc85@gmail.com>
2021-08-19 22:06:47 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@ use winapi::um::winnt::{GENERIC_READ, FILE_SHARE_READ,FILE_ATTRIBUTE_NORMAL};
 use winapi::um::fileapi::CREATE_NEW;
 
 fn main() {
-	let file_name = CString::new("C:\\Users\\DummyUser\\Desktop\\test.txt").unwrap();
+    let file_name = CString::new("C:\\Users\\DummyUser\\Desktop\\test.txt").unwrap();
 	
     unsafe {
         CreateFileA(
-			file_name.as_ptr(),
-			GENERIC_READ,
-			FILE_SHARE_READ,
-            		std::ptr::null_mut(),
-			CREATE_NEW,
-			FILE_ATTRIBUTE_NORMAL,
-			std::ptr::null_mut()
+            file_name.as_ptr(),
+            GENERIC_READ,
+            FILE_SHARE_READ,
+            std::ptr::null_mut(),
+            CREATE_NEW,
+            FILE_ATTRIBUTE_NORMAL,
+            std::ptr::null_mut()
         );
     }
 }
